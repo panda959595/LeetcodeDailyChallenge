410b5adde5cc16524426511bdac7c2e2a03d167d
pinder <panda959595@naver.com>
2021-05-25 12:48:01 +0900

    Create 5_25.h

---
+++ 5_25.h
@@ -0,0 +1,13 @@
+#include <string>
+using namespace std;
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] >= 'A' && s[i] <= 'Z') {
+                s[i] -= 'A' - 'a';
+            }
+        }
+        return s;
+    }
+};
